dbb29361ea9702168921e0dab0d9c87eda96dc38
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2020-11-21 14:06:17 -0700

    cha cha changes

--- src/sequalize.js
+++ src/sequalize.js
@@ -1,14 +1,16 @@
 const { Sequelize, QueryTypes } = require('sequelize');
 
-const db = new Sequelize(
-  process.env.POSTGRES_DATABASE,
-  process.env.POSTGRES_USERNAME,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: 'roa-postgres',
-    port: process.env.POSTGRES_PORT,
-    dialect: 'postgres',
-  }
-);
+const db = PROCESS.env.DATABASE_URL
+  ? PROCESS.env.DATABASE_URL
+  : new Sequelize(
+      process.env.POSTGRES_DATABASE,
+      process.env.POSTGRES_USERNAME,
+      process.env.POSTGRES_PASSWORD,
+      {
+        host: 'roa-postgres',
+        port: process.env.POSTGRES_PORT,
+        dialect: 'postgres',
+      }
+  );
 
 module.exports = { QueryTypes, db };
