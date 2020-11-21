ced493849d8124c8e94bf95a7d20333b13630b71
Kristopher Paulsen <kristopherpaulsen@gmail.com>
2020-11-21 14:53:01 -0700

    cha cha changes

--- src/sequalize.js
+++ src/sequalize.js
@@ -1,9 +1,5 @@
 const { Sequelize, QueryTypes } = require('sequelize');
 
-console.log(process.env.DATABASE_URL);
-console.log(process.env.DATABASE_URL);
-console.log(process.env.DATABASE_URL);
-console.log(process.env.DATABASE_URL);
 const match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
 const db = new Sequelize(match[5], match[1], match[2], {
     dialect:  'postgres',
